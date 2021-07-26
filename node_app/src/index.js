4aaa38ac0b94d38a76e7e2b7f27880a4f7d4ff56
kryz <kpiechowicz@gmail.com>
2021-07-26 17:38:05 +0200

    build node app image and push to docker hub

---
+++ node_app/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("OK");
+})
+
+app.listen(4000, () => {
+  console.log("Listening on 4000...");
+})
